1ad404c5ef80081d8b6e5745c74bf299d17775c3
Baptiste Leroux <lerouxbaptistepro2@gmail.com>
2026-03-03 17:52:32 +0100

    Added YAML file configuration

--- configuration.py
+++ configuration.py
@@ -1,5 +1,6 @@
 import torch
 import os
+import yaml
 
 """
 LINKS: 
@@ -8,16 +9,33 @@ dataset_path : https://huggingface.co/datasets/blanchon/INRIA-Aerial-Image-Label
 
 # Torch settings
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-TORCH_MODEL_PATH = "D:/models/torch/hub"
-torch.hub.set_dir(TORCH_MODEL_PATH) # Un/comment this line to un/set the directory to download pytorch models
+TORCH_MODEL_PATH = "D:/models/torch/hub"  # Update path for your need, this is where pytorch models will be downloaded and stored
+torch.hub.set_dir(TORCH_MODEL_PATH) # Un/comment this line to un/set the previous directory
 
 # Paths
 MAIN_DATA_PATH = "D:/Programmation/IA/datas/AerialImageDataset"
 MODEL_WEIGHTS_PATH = os.path.join(os.curdir, "models")
 
+# Load config
+config_path = os.path.join(os.curdir, "config.yaml")
+with open(config_path, "r") as f:
+    config = yaml.safe_load(f)
+
 # Hyperparameters & Global settings
-PATCH_SIZE = 128
-BATCH_SIZE = 16
-EPOCHS = 35
-LR_BACKBONE = 1e-5
-LR_CLASSIFIER = 1e-3
+PATCH_SIZE = config["settings"]["patch_size"]
+
+BATCH_SIZE = config["hyperparameters"]["batch_size"]
+EPOCHS = config["hyperparameters"]["epochs"]
+LR_BACKBONE = config["hyperparameters"]["lr_backbone"]
+LR_CLASSIFIER = config["hyperparameters"]["lr_classifier"]
+
+
+if __name__ == "__main__":
+    print(f"Device: {DEVICE}")
+    print(f"Main data path: {MAIN_DATA_PATH}")
+    print(f"Model weights path: {MODEL_WEIGHTS_PATH}")
+    print(f"Patch size: {PATCH_SIZE}")
+    print(f"Batch size: {BATCH_SIZE}")
+    print(f"Epochs: {EPOCHS}")
+    print(f"Learning rate for backbone: {LR_BACKBONE}")
+    print(f"Learning rate for classifier: {LR_CLASSIFIER}")
